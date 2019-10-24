ada17e796013989a3d28b74ea2a0a17f5445526a
yuhzen <yuhzen@gmail.com>
2019-10-24 09:15:03 +0000

    Replace with try-catch

--- php/bench-base.php
+++ php/bench-base.php
@@ -1,7 +1,20 @@
 <?php
-$start_ms = microtime(true);
+// Benchmark Base File on PHP
+$f_start_ms = microtime(true);
+$f_diff_ms  = (float)0;
+//$f_diff_ms2  = (float)0;
+ini_set('error_log', 'php://stderr');
 
-// @todo
-
-$diff_ms = microtime(true) - $start_ms;
-echo "{$diff_ms} micro-sec";
+try {
+    // @todo
+    throw new Exception('@ToDo: Write test code.');
+}
+catch(Exception $e) {
+    error_log("**Exception**: {$e->getMessage()}");
+}
+finally {
+    $f_diff_ms = microtime(true) - $f_start_ms;
+    //$f_diff_ms2 = microtime(true) - $_SERVER{'REQUEST_TIME_FLOAT'};
+    error_log("{$f_diff_ms} micro-sec.");
+    //error_log("{$f_diff_ms2} micro-sec.");
+}
